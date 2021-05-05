4bd63d8c4790ad5516f0705810b332435e10dbf8
garrystewart <garry.stewart@glamis.casa>
2021-05-05 22:05:35 +0100

    wip

--- r710_fan_control/Controllers/HomeController.cs
+++ r710_fan_control/Controllers/HomeController.cs
@@ -23,13 +23,58 @@ namespace r710_fan_control.Controllers
         private const string _password = "calvin";
         private static readonly string _baseCommand = $"{_ipmiToolPath}ipmitool -I lanplus -H {_hostname} -U {_user} -P {_password}";
         private static readonly string _raw = $"{_baseCommand} raw";
+
         public async Task<ActionResult> Index()
+        {
+            //string json = await _client.GetStringAsync(_url);
+
+            //Data data = Data.FromJson(json);
+
+            //Stats stats = new Stats();
+
+            //ICollection<string> temperatureList = new List<string>();
+
+            //IEnumerable<Data> processors = data.Children.Where(c => c.Text == "WIN-USMAMP0H8B8").Single().Children
+            //    .Where(c => c.Text == "Intel Xeon L5640").ToList();
+
+            //foreach (var processor in processors)
+            //{
+            //    var processorToAdd = new Stats.Processor { Name  =processor.Text };
+
+            //    foreach (var temperature in processors
+            //        .SelectMany(p => p.Children
+            //        .Where(c => c.Text == "Temperatures")
+            //        .SelectMany(t => t.Children)))
+            //    {
+            //        processorToAdd.Cores.Add(temperature.Value);
+            //    }
+
+            //    stats.Processors.Add(processorToAdd);
+            //}
+
+            //foreach (var temperature in processors
+            //    .SelectMany(processor => processor.Children
+            //    .Where(c => c.Text == "Temperatures")
+            //    .SelectMany(t => t.Children)))
+            //{
+            //    temperatureList.Add(temperature.Value);
+            //}
+
+            return View(GetHighestTemperature(await GetTemperatures()));
+        }
+
+        public decimal GetHighestTemperature(IEnumerable<decimal> temperatures)
+        {
+            return temperatures.Max();
+        }
+
+        public async Task<IEnumerable<decimal>> GetTemperatures()
         {
             string json = await _client.GetStringAsync(_url);
 
             Data data = Data.FromJson(json);
 
-            ICollection<string> temperatureList = new List<string>();
+            ICollection<decimal> temperatures = new List<decimal>();
 
             IEnumerable<Data> processors = data.Children.Where(c => c.Text == "WIN-USMAMP0H8B8").Single().Children
                 .Where(c => c.Text == "Intel Xeon L5640").ToList();
@@ -39,10 +84,10 @@ namespace r710_fan_control.Controllers
                 .Where(c => c.Text == "Temperatures")
                 .SelectMany(t => t.Children)))
             {
-                temperatureList.Add(temperature.Value);
+                temperatures.Add(decimal.Parse(temperature.Value.Replace(" °C", "")));
             }
 
-            return View(temperatureList);
+            return temperatures;
         }
 
         [HttpGet]
@@ -59,25 +104,42 @@ namespace r710_fan_control.Controllers
                 throw new ArgumentNullException(nameof(model));
             }
 
-            if (model.Speed == "auto")
-            {
-                Command($"{_raw} 0x30 0x30 0x01 0x01");
-            }
-            else
-            {
-                string hexSpeed = int.Parse(model.Speed).ToString("x");
+            string hexSpeed = int.Parse(model.Speed).ToString("x");
 
+            if (MvcApplication.State != State.Manual)
+            {
                 Command($"{_raw} 0x30 0x30 0x01 0x00");
-                Command($"{_raw} 0x30 0x30 0x02 0xff 0x{hexSpeed}");
+                MvcApplication.State = State.Manual;
             }
+
+            Command($"{_raw} 0x30 0x30 0x02 0xff 0x{hexSpeed}");
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult FanAuto()
+        {
+            if (MvcApplication.State != State.Auto)
+            {
+                Command($"{_raw} 0x30 0x30 0x01 0x01");
+                MvcApplication.State = State.Auto;
+            }
+
+            return RedirectToAction("Fan");
         }
 
         private static void Command(string command)
         {
             Process.Start("cmd.exe", $"/C {command}");
         }
+    }
+
+    public enum State
+    {
+        None,
+        Auto,
+        Manual
     }
 
     public partial class Data
--- r710_fan_control/Global.asax.cs
+++ r710_fan_control/Global.asax.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using r710_fan_control.Controllers;
 
 namespace r710_fan_control
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static State State { get; set; }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
