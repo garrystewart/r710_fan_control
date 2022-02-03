6dcc6f898072dcdb2ec03dce8abd33f239e5722e
Garry Stewart <garry.stewart@glamis.casa>
2022-02-03 22:21:13 +0000

    added in mode average for fans and restructured

--- r710_fan_control_core/Controllers/HAController.cs
+++ r710_fan_control_core/Controllers/HAController.cs
@@ -20,38 +20,59 @@ namespace r710_fan_control_core.Controllers
 
             var power = new List<HomeAssistant.PowerType>();
 
-            var current = sensors.Single(s => 
+            var current = sensors.Single(s =>
                 s.ProbeName == "Current" && s.Measurement != Measurement.None);
 
-            power.Add(new HomeAssistant.PowerType { 
-                Name = "Current", 
-                Reading = Convert.ToDecimal(current.Reading), 
-                Measurement = "Amps" });
+            power.Add(new HomeAssistant.PowerType
+            {
+                Name = "Current",
+                Reading = Convert.ToDecimal(current.Reading),
+                Measurement = "Amps"
+            });
 
 
-            var voltage = sensors.Single(s => 
+            var voltage = sensors.Single(s =>
                 s.ProbeName == "Voltage" && s.Measurement != Measurement.None);
 
-            power.Add(new HomeAssistant.PowerType { 
-                Name = "Voltage", 
-                Reading = Convert.ToDecimal(voltage.Reading), 
-                Measurement = "Volts" });
+            power.Add(new HomeAssistant.PowerType
+            {
+                Name = "Voltage",
+                Reading = Convert.ToDecimal(voltage.Reading),
+                Measurement = "Volts"
+            });
 
 
             var system = sensors.Single(s => s.ProbeName == "System Level");
 
-            power.Add(new HomeAssistant.PowerType { 
-                Name = "Watts", 
-                Reading = Convert.ToDecimal(system.Reading), 
-                Measurement = "Watts" });
+            power.Add(new HomeAssistant.PowerType
+            {
+                Name = "Watts",
+                Reading = Convert.ToDecimal(system.Reading),
+                Measurement = "Watts"
+            });
 
             return new HomeAssistant()
             {
-                Fans = sensors.Where(s => s.ProbeName.Contains("FAN"))
-                    .Select(s => new HomeAssistant.Fan() {
-                        Reading = Convert.ToInt32(Convert.ToDecimal(s.Reading)), 
-                        Measurement = Enum.GetName(s.Measurement) })
-                    .ToList(),
+                Fans = new HomeAssistant.FanType
+                {
+                    FansList = sensors.Where(s => s.ProbeName.Contains("FAN"))
+                        .Select(s => new HomeAssistant.Fan
+                        {
+                            Reading = Convert.ToInt32(Convert.ToDecimal(s.Reading)),
+                            Measurement = Enum.GetName(s.Measurement)
+                        })
+                        .ToList(),
+                    FansModeAverage = new HomeAssistant.Fan
+                    {
+                        Reading = sensors
+                        .Where(s => s.ProbeName.Contains("FAN"))
+                        .GroupBy(n => Convert.ToInt32(Convert.ToDecimal(n.Reading)))
+                        .OrderByDescending(g => g.Count())
+                        .Select(g => g.Key)
+                        .FirstOrDefault(),
+                        Measurement = "RPM"
+                    }
+                },
                 Power = power
             };
         }
--- r710_fan_control_core/Models/API/HomeAssistant.cs
+++ r710_fan_control_core/Models/API/HomeAssistant.cs
@@ -4,8 +4,15 @@ namespace r710_fan_control_core.Models.API
 {
     public class HomeAssistant
     {
-        public IEnumerable<Fan> Fans { get; set; }
+        public FanType Fans { get; set; }
+
         public IEnumerable<PowerType> Power { get; set; }
+
+        public class FanType
+        {
+            public IEnumerable<Fan> FansList { get; set; }
+            public Fan FansModeAverage { get; set; }
+        }
 
         public class Fan
         {
